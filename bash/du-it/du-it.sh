7e51fb7074dea5c1d6159a3f1b7c3447ca58126c
kamahell87 <marco.daleo@outlook.com>
2022-05-30 15:41:45 +0100

    autoformatted

--- bash/du-it/du-it.sh
+++ bash/du-it/du-it.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-
 BRed='\033[1;31m'
 BGreen='\033[1;32m'
 BYellow='\033[1;33m'
@@ -12,64 +11,57 @@ Yellow='\033[0;93m'
 Cyan='\033[0;96m'
 NC='\033[0m'
 
-
 echo -e "Choose one of the following options:\n${BRed}(1) Delete${NC} ${BCyan}(2) Update${NC} ${BYellow}(3) Quit${NC} "
 read ACTION
 
-if [[ $ACTION -eq 1 ]]
-then
-	for n in $*
-	do
-		
-		if [[ "$n" == "$(sudo docker ps -a | grep $n | awk '{ print $NF }')" ]]
-		then
-				
-			sudo docker ps -a | grep $n | awk '{ print $2 }' > /dev/null > $n.tmp
+if [[ $ACTION -eq 1 ]]; then
+	for n in $*; do
+
+		if [[ "$n" == "$(sudo docker ps -a | grep $n | awk '{ print $NF }')" ]]; then
+
+			sudo docker ps -a | grep $n | awk '{ print $2 }' >/dev/null >$n.tmp
 			echo -e "${Red}Stopping $n...${NC}"
-			sudo docker stop $n > /dev/null
+			sudo docker stop $n >/dev/null
 			echo -e "${BGreen}Done!${NC}"
 			echo -e "${Red}Removing $n...${NC}"
-			sudo docker rm $n > /dev/null
+			sudo docker rm $n >/dev/null
 			echo -e "${BGreen}Done!${NC}"
 			echo -e "${Red}Removing the image used by $n...${NC}"
-			sudo docker rmi $(cat $n.tmp) > /dev/null
+			sudo docker rmi $(cat $n.tmp) >/dev/null
 			echo -e "${BGreen}Done!${NC}"
 
 			echo -e "${Red}Cleaning up...${NC}"
 			rm *.tmp
 			echo -e "${BGreen}Done!${NC}"
-			
+
 		else
 			echo -e "${BRed}$n doesn't exist!${NC}"
 		fi
 	done
 
-elif [[ $ACTION -eq 2 ]]
-then	
-	for n in $*
-	do
-		
-		if [[ "$n" == "$(sudo docker ps -a | grep $n | awk '{ print $NF }')" ]] 
-		then
-		
-			sudo docker ps -a | grep $n | awk '{ print $2 }' > /dev/null > $n.tmp
+elif [[ $ACTION -eq 2 ]]; then
+	for n in $*; do
+
+		if [[ "$n" == "$(sudo docker ps -a | grep $n | awk '{ print $NF }')" ]]; then
+
+			sudo docker ps -a | grep $n | awk '{ print $2 }' >/dev/null >$n.tmp
 			echo -e "${Cyan}Stopping $n...${NC}"
-			sudo docker stop $n > /dev/null
+			sudo docker stop $n >/dev/null
 			echo -e "${Green}Done!${NC}"
 			echo -e "${Cyan}Removing $n...${NC}"
-			sudo docker rm $n > /dev/null
+			sudo docker rm $n >/dev/null
 			echo -e "${Green}Done!${NC}"
 			echo -e "${Cyan}Removing the image used by $n...${NC}"
-			sudo docker rmi $(cat $n.tmp) > /dev/null
+			sudo docker rmi $(cat $n.tmp) >/dev/null
 			echo -e "${Green}Done!${NC}"
 
 			echo -e "${Cyan}Creating $n...${NC}"
-			sudo docker-compose -f ~/$n/docker-compose.yml up -d > /dev/null
+			sudo docker-compose -f ~/$n/docker-compose.yml up -d >/dev/null
 
 			echo -e "${Cyan}Cleaning up...${NC}"
 			rm *.tmp
 			echo -e "${Green}Done!${NC}"
-	
+
 		else
 			echo -e "${BRed}$n doesn't exist!${NC}"
 		fi
