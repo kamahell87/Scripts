46c69febba4f7906b6ffa9fde31a9d9468ff8c77
kamahell87 <marco.daleo@outlook.com>
2022-10-29 10:03:37 +0100

    minor changes

--- bash/du-it/du-it.sh
+++ bash/du-it/du-it.sh
@@ -20,17 +20,17 @@ if [[ $ACTION -eq 1 ]]; then
 		if [[ "$n" == "$(sudo docker ps -a | grep $n | awk '{ print $NF }')" ]]; then
 
 			sudo docker ps -a | grep $n | awk '{ print $2 }' >/dev/null >$n.tmp
-			echo -e ">>>${Red}Stopping $n...${NC}"
+			echo -e "${Red}>>> Stopping $n...${NC}"
 			sudo docker stop $n >/dev/null
 			echo -e "${BGreen}Done!${NC}"
-			echo -e ">>>${Red}Removing $n...${NC}"
+			echo -e "${Red}>>> Removing $n...${NC}"
 			sudo docker rm $n >/dev/null
 			echo -e "${BGreen}Done!${NC}"
-			echo -e ">>>${Red}Removing the image used by $n...${NC}"
+			echo -e "${Red}>>> Removing the image used by $n...${NC}"
 			sudo docker rmi $(cat $n.tmp) >/dev/null
 			echo -e "${BGreen}Done!${NC}"
 
-			echo -e "$>>>{Red}Cleaning up...${NC}"
+			echo -e "${Red}>>> Cleaning up...${NC}"
 			rm *.tmp
 			echo -e "${BGreen}Done!${NC}"
 
@@ -45,20 +45,20 @@ elif [[ $ACTION -eq 2 ]]; then
 		if [[ "$n" == "$(sudo docker ps -a | grep $n | awk '{ print $NF }')" ]]; then
 
 			sudo docker ps -a | grep $n | awk '{ print $2 }' >/dev/null >$n.tmp
-			echo -e "$>>>{Cyan}Stopping $n...${NC}"
+			echo -e "${Cyan}>>> Stopping $n...${NC}"
 			sudo docker stop $n >/dev/null
 			echo -e "${Green}Done!${NC}"
-			echo -e "$>>>{Cyan}Removing $n...${NC}"
+			echo -e "${Cyan}>>> Removing $n...${NC}"
 			sudo docker rm $n >/dev/null
 			echo -e "${Green}Done!${NC}"
-			echo -e "$>>>{Cyan}Removing the image used by $n...${NC}"
+			echo -e "${Cyan}>>> Removing the image used by $n...${NC}"
 			sudo docker rmi $(cat $n.tmp) >/dev/null
 			echo -e "${Green}Done!${NC}"
 
-			echo -e "$>>>{Cyan}Creating $n...${NC}"
+			echo -e "${Cyan}>>> Creating $n...${NC}"
 			sudo docker-compose -f ~/$n/docker-compose.yml up -d >/dev/null
 
-			echo -e "$>>>{Cyan}Cleaning up...${NC}"
+			echo -e "${Cyan}>>> Cleaning up...${NC}"
 			rm *.tmp
 			echo -e "${Green}Done!${NC}"
 
@@ -70,12 +70,12 @@ elif [[ $ACTION -eq 2 ]]; then
 elif [[ $ACTION -eq 3 ]]; then
 	for n in $*; do
 
-		echo -e "${Yellow}>>>Navigating to $n...${NC}"
+		echo -e "${Yellow}>>> Navigating to $n...${NC}"
 		builtin cd ~/$n/
-		echo -e "${Yellow}>>>Pulling the new image...${NC}"
+		echo -e "${Yellow}>>> Pulling the new image...${NC}"
 		sudo docker-compose pull > /dev/null
 		echo -e "${Green}Done!${NC}"
-		echo -e "${Yellow}>>>Recreating $n...${NC}"
+		echo -e "${Yellow}>>> Recreating $n...${NC}"
 		sudo docker-compose up -d > /dev/null
 		echo -e "${Green}Done!${NC}"
 
