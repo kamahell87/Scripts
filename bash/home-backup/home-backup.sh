702f9c2d89200d6c4c63f33c44153b79973feda6
kamahell87 <marco.daleo@outlook.com>
2023-06-01 10:51:25 +0100

    add new scripts

---
+++ bash/home-backup/home-backup.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# Written by Marco D'Aleo
+
+usage()
+{
+cat << EOF
+
+Script to take backup of /home directory.
+
+ARGUMENTS:
+  -d    The device name. e.g. tank, personal
+  -u    The /home owner.
+  -h    This help message.
+
+Syntax: home-backup.sh -d [tank, personal] -u [dominus, kamahell87]
+
+EOF
+  exit 1
+}
+
+while getopts "d:u:h" OPTION; do
+  case $OPTION in
+    h)
+      usage
+      exit 0
+      ;;
+    u)
+      USER=$OPTARG
+      ;;
+    d)
+      DEVICE=$OPTARG
+      ;;
+    *)
+      usage
+      exit 1
+      ;;
+  esac
+done
+
+# Syntax check
+if [[ $# -eq 0 ]]; then
+  usage
+  exit 1
+fi
+
+
+do_the_magic()
+{
+# Owner check
+if [[ ! -d /home/$USER ]]; then
+  echo "Oops! Apparently '$USER' doesn't have a home!"
+  exit 1
+fi
+
+DIR=./$DEVICE-backups
+
+if [[ ! -d $DIR ]]; then
+  mkdir $DIR
+fi
+
+echo ">>> Creating the tarball..."
+tar -zcpf $DIR/$DEVICE-backup-$(date +%d-%m-%Y).tar /home/$USER
+
+echo ">>> bzip-ing the tarball..."
+bzip2 $DIR/$DEVICE-backup-$(date +%d-%m-%Y).tar
+
+echo ">>> The following backup files have been removed (older than 60 days)."
+find $DIR/ -name '*.bz2' -type f -mtime +60 -delete -print
+}
+
+do_the_magic
---
+++ python/ansible-role-checker/myarc.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+
+# My Ansible Roles Checker
+# Written by Marco D'Aleo
+
+import os
+import yaml
+import argparse
+
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "-s",
+    "--source",
+    type=str,
+    dest="source",
+    help="Ansible root folder. e.g. ~/somedir/ansible",
+)
+
+args = parser.parse_args()
+
+
+roles_ls = [
+    f
+    for f in os.listdir(f"{args.source}/roles/")
+    if not f.startswith(".")
+]
+
+
+playbooks = [
+    f
+    for f in os.listdir(f"{args.source}/playbooks/")
+    if not f.startswith(".")
+]
+
+
+# How many roles do we have?
+print(f"\n >>> Checking {len(roles_ls)} roles... \n")
+
+roles_used_list = []
+roles_doubles_list = []
+
+for d in roles_ls:
+    for p in playbooks:
+        with open(
+            f"{args.source}/playbooks/{p}"
+        ) as f:
+            full_file = yaml.safe_load(f)
+            for item in full_file:
+                for i in item:
+                    # Find the role
+                    if i == "roles":
+                        roles = item[i]
+                        for r in roles:
+                            # Checking...
+                            if r == d:
+                                if r not in roles_used_list:
+                                    roles_used_list.append(r)
+                                else:
+                                    if r not in roles_doubles_list:
+                                        roles_doubles_list.append(r)
+                                print(f"Found role '{r}' in playbook '{p}'")
+
+# Comparing...
+roles_unused_list = [r for r in roles_ls if r not in roles_used_list]
+if roles_used_list == roles_ls:
+    print(" All roles are used in at least one playbook!")
+
+if roles_unused_list:
+    print(f" {roles_unused_list} not used by any playbook")
+
+if roles_doubles_list:
+    print(f" {roles_doubles_list} referenced in multiple playbooks!")
+
